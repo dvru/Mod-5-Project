ab28c4c23c125e285a51b78255f2f7c502c37f8a
Damini varu <daminivaru@Daminis-MBP.attlocal.net>
2019-12-26 21:57:44 -0600

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,57 +14,57 @@ import LoginPage from './LoginPage'
 
 class App extends React.Component {
 
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.state = {
-      user: []
-    }
-  }
+  //   this.state = {
+  //     user: []
+  //   }
+  // }
 
-logout = () => {
-  localStorage.clear();
-  localStorage.firstName = this.state.defaultUser.firstName;
-  localStorage.lastName = this.state.defaultUser.lastName;
-  localStorage.age = this.state.defaultUser.age;
-  localStorage.bio = this.state.defaultUser.bio;
-  this.setState({
-    user: this.state.defaultUser
-  })
-  // console.log(localStorage);
-}
+// logout = () => {
+//   localStorage.clear();
+//   localStorage.firstName = this.state.defaultUser.firstName;
+//   localStorage.lastName = this.state.defaultUser.lastName;
+//   localStorage.age = this.state.defaultUser.age;
+//   localStorage.bio = this.state.defaultUser.bio;
+//   this.setState({
+//     user: this.state.defaultUser
+//   })
+//   // console.log(localStorage);
+// }
 
-login = (e) => {
-  e.preventDefault();
+// login = (e) => {
+//   e.preventDefault();
 
-  fetch('http://localhost:3000/users/login', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: e.target[0].value,
-      password: e.target[1].value
-    })
-  }).then(res => res.json())
-  .then(user => {
-    if (user.status === 'success') {
-      localStorage.token = user.token;
-      localStorage.username = user.username;
-      localStorage.firstName = user.firstName;
-      localStorage.lastName = user.lastName;
-      localStorage.email = user.email;
-      localStorage.age = user.age;
-      localStorage.issues = user.issues;
-      localStorage.q = user.q;
-      // localStorage = user;
-      console.log(user);
-      this.setState({
-        user 
-      })
-    }
-  });
-}
+//   fetch('http://localhost:3000/users/login', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//       username: e.target[0].value,
+//       password: e.target[1].value
+//     })
+//   }).then(res => res.json())
+//   .then(user => {
+//     if (user.status === 'success') {
+//       localStorage.token = user.token;
+//       localStorage.username = user.username;
+//       localStorage.firstName = user.firstName;
+//       localStorage.lastName = user.lastName;
+//       localStorage.email = user.email;
+//       localStorage.age = user.age;
+//       localStorage.issues = user.issues;
+//       localStorage.q = user.q;
+//       // localStorage = user;
+//       console.log(user);
+//       this.setState({
+//         user 
+//       })
+//     }
+//   });
+// }
 
 
 
